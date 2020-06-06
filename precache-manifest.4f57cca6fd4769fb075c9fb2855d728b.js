d5acf0c0355e18c35235b28468a9bb3b1e551aaf
aliksayf <52054450+aliksayf@users.noreply.github.com>
2020-06-06 09:28:35 -0700

    Updates

---
+++ precache-manifest.4f57cca6fd4769fb075c9fb2855d728b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6277bd5315492fc0746e836e180c9bb",
+    "url": "/react-kanban-board-2.0/index.html"
+  },
+  {
+    "revision": "7e6a5aa59cba0c1e98ea",
+    "url": "/react-kanban-board-2.0/static/css/2.22cdf902.chunk.css"
+  },
+  {
+    "revision": "16c8402e94a2b1295ee2",
+    "url": "/react-kanban-board-2.0/static/css/main.04bce781.chunk.css"
+  },
+  {
+    "revision": "7e6a5aa59cba0c1e98ea",
+    "url": "/react-kanban-board-2.0/static/js/2.5173a38e.chunk.js"
+  },
+  {
+    "revision": "a7c4b35d19305ff13a649f6cb3b1191a",
+    "url": "/react-kanban-board-2.0/static/js/2.5173a38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c8402e94a2b1295ee2",
+    "url": "/react-kanban-board-2.0/static/js/main.63ea2c30.chunk.js"
+  },
+  {
+    "revision": "ee16fe5b8751bf1abc37",
+    "url": "/react-kanban-board-2.0/static/js/runtime-main.76afe9d2.js"
+  }
+]);
